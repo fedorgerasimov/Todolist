86d138990ca11b53dd852b33979531b31849e232
fedorgerasimov <1031577@mail.ru>
2022-03-09 19:55:28 +0300

    добавил хук - let [valueButton, setValueButton] = useState( "All")

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,30 @@ function App() {
     let [tasks1, setTasks1] = useState( [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "NodeJS", isDone: false }
     ])
-    const removedTask = (id:number) => {
-        let filtered = tasks1.filter(el=>el.id !== id)
+    const removedTask = (NewId:number) => {
+        let filtered = tasks1.filter(el=>el.id !== NewId)
         setTasks1(filtered)
     }
-    const tasksFiler = (filterValue:string) => {
-        console.log(filterValue)
-    }
 
+    let [valueButton, setValueButton] = useState( "All")
+    const tasksFiler = (filterValue:string) => {
+        setValueButton(filterValue)
+    }
+    let prokladka = tasks1
+    if (valueButton === "Active") {
+        prokladka = tasks1.filter(el=> el.isDone === false)
+    }
+    if (valueButton === "Completed") {
+        prokladka = tasks1.filter(el=> el.isDone === true)
+    }
 
     return (
         <div className="App">
             <Todolist title="What to learn"
-                      tasks={tasks1}
+                      tasks={prokladka}
                       removedTask = {removedTask}
                       tasksFilter={tasksFiler}
             />
