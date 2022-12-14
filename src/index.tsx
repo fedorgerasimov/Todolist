5f5dbd0cda0744c39caaa700c348c7b8f4c1ff21
fedorgerasimov <1031577@mail.ru>
2022-12-14 14:33:51 +0300

    13. installed new files from archive of 13/ old files deleted

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
-import {darken, ThemeProvider} from "@material-ui/core/styles";
-import {createTheme} from '@material-ui/core/styles';
-import {blueGrey, deepOrange} from "@material-ui/core/colors";
-import {CssBaseline} from "@material-ui/core";
-import AppWithRedux from "./components/App/AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "./store/store";
-import AppWithReduxWithoutProps from "./TodolistWithoutProps/AppWithReduxWithoutProps";
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './state/store';
 
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 
-const theme = createTheme({
-    palette: {
-        primary: blueGrey,
-        secondary: deepOrange,
-        type: "light"
-    }
-})
-
-ReactDOM.render(
+root.render(
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline/> {/*сбрасывает стили (сбрасывает background)*/}
-            <AppWithRedux/>
-        </ThemeProvider>
+    <App />
     </Provider>
-    , document.getElementById('root'));
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
